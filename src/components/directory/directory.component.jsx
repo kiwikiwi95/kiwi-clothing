655fc4dc4b8a81dd18fda999bfc8318f33ce10d2
Kiwi <jc1887@rutgers.edu>
2020-05-06 14:47:57 -0400

    add header component and links to home, shop page and contact

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,11 +49,9 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {
-          this.state.sections.map(({id, ...otherSectionProps}) => (
-            <MenuItem key={id} {...otherSectionProps} />
-          ))
-        }
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
       </div>
     );
   }
