417f008448b5917ba3e7436857ab31479f73859b
Kiwi <jc1887@rutgers.edu>
2020-05-06 14:26:07 -0400

    update our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import { Route } from "react-router-dom";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop/hats" component={HatsPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,15 +5,15 @@ import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
-  <div 
+  <div
     className={`${size} menu-item`}
     onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
-    <div 
-      className="background-image" 
+    <div
+      className="background-image"
       style={{
-        backgroundImage: `url(${imageUrl})`
-      }} 
+        backgroundImage: `url(${imageUrl})`,
+      }}
     />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
